e68e5150b5907926747bbb7bc95070bea095b40e
Nodirbek Mamadaliyev <nodirbekmamadliyev>
2021-10-05 11:26:30 +0500

    first commit

---
+++ src/main/java/uz/tuit/vediocall/VedioCallApplication.java
@@ -0,0 +1,13 @@
+package uz.tuit.vediocall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VedioCallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VedioCallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/tuit/vediocall/VedioCallApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.tuit.vediocall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VedioCallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
